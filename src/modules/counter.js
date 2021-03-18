4e1eaec1d0ca27694a79e69d794207b8250f4452
aboiko1281 <aboiko1281@gmail.com>
2021-03-18 14:53:26 +0200

    10 to 20 conversion

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -136,19 +136,49 @@ export const loadCharacterListAsyncSelectedPage = (numberPage) => {
   };
 };
 
-export const loadCharacterListAsyncFilter = (filtersString, pageNumber = 1) => {
-  const pageAndFilterUrlPath = 'page=' + pageNumber + '&' + filtersString;
+const pageBy20FromBy10Number = (pageNumberBy10) => {
+  const itemIndexOfPageStart = (pageNumberBy10 - 1) * 10;
+  const pageNumberBy20 = Math.floor((itemIndexOfPageStart / 20)) + 1;
+  return pageNumberBy20;
+};
+
+const page10ItemsFromPage20Results = (pageBy20Items, pageNumberBy10) => {
+  console.log('pageBy20Items', pageBy20Items);
+  console.log('pageNumberBy10', pageNumberBy10);
+  const isFirst10Items = pageNumberBy10 % 2 === 1;
+  const pageBy10Items = isFirst10Items ? pageBy20Items.slice(0, 10) :  pageBy20Items.slice(10, 20);
+  return pageBy10Items;
+}
+
+export const loadCharacterListAsyncFilter = (filtersString = '', pageBy10Number = 1) => {
+
+  const pageBy20Number = pageBy20FromBy10Number(pageBy10Number);
+  const pageAndFilterUrlPath = 'page=' + pageBy20Number + '&' + filtersString;
 
   return async (dispatch) => {
     dispatch({
       type: CHARACTER_LIST_REQUESTED_FILTER,
     })
-    const list = await api.getListFilter(pageAndFilterUrlPath);
 
-    console.log('loadCharacterListAsyncFilter list', list)
+    console.log('pageAndFilterUrlPath', pageAndFilterUrlPath);
+
+    // const list = await api.getListFilter(pageAndFilterUrlPath);
+    const listBy20 = await api.getListFilter(pageAndFilterUrlPath);
+
+    const pageItemsBy10 = page10ItemsFromPage20Results(listBy20.results, pageBy10Number);
+
+    const listBy10 = {
+      info: {
+        count: listBy20.info.count,
+        pages: Math.ceil(listBy20.info.count / 10),
+      },
+      results: pageItemsBy10,
+    };
+    console.log('loadCharacterListAsyncFilter listBy20', listBy20)
+    console.log('loadCharacterListAsyncFilter listBy10', listBy10)
     dispatch({
       type: CHARACTER_LIST_FILTER,
-      payload: list,
+      payload: listBy10,
       filter: filtersString,
     })
   };
